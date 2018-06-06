80ad6dd3f8e4c5ab87dc889cd30b314bc77cdcc1
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-06 21:00:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -10,8 +10,8 @@
 
 def prime?(n)
   #Yet another way to say the same thing is that a number n is prime if it is greater than one and if none of the numbers divides n evenly
-  if n > 1 && n % 2 != 0
-    return true
+  if n <= 0 || n == 1
+    return false
   else
     return false
   end
