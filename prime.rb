2c0a3113037d28725c97f60238b9889ad32b58c1
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-06 21:03:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -12,7 +12,7 @@ def prime?(n)
   #Yet another way to say the same thing is that a number n is prime if it is greater than one and if none of the numbers divides n evenly
   if n <= 0 || n == 1
     return false
-  elsif (2..n-1).to_a.all? do |x|
+  else (2..n-1).to_a.all? do |x|
     n % x != 0
     end
   end
